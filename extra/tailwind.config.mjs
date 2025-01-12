594a95823a63ce227a7be0732960baa44c9c2807
MrBoxs <rizkywanda200155@gmail.com>
2025-01-13 04:17:46 +0700

    extra lesson & execise 02 (button) with tailwindcss

---
+++ extra/tailwind.config.mjs
@@ -0,0 +1,11 @@
+export default {
+  content: ["./src/**/*.{html,js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        inter: ["Inter", "Avenir", "Helvetica", "Arial", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
